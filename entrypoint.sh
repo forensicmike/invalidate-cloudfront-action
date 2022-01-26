911f19b46b0c74320d8477ad08743eacb750b38f
forensicmike1 <mike@forensicmike1.com>
2022-01-26 16:21:45 -0700

    Test removal of AWS AKID and region requirements as well as profile creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,36 +16,11 @@ if [[ -z "$PATHS" && -z "$PATHS_FROM" ]]; then
   err=1
 fi
 
-if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-  echo "error: AWS_ACCESS_KEY_ID is not set"
-  err=1
-fi
-
-if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "error: AWS_SECRET_ACCESS_KEY is not set"
-  err=1
-fi
-
-if [ -z "$AWS_REGION" ]; then
-  echo "error: AWS_REGION is not set"
-  err=1
-fi
-
 if [ $err -eq 1 ]; then
   exit 1
 fi
 
 # run
-
-# Create a dedicated profile for this action to avoid
-# conflicts with other actions.
-# https://github.com/jakejarvis/s3-sync-action/issues/1
-aws configure --profile invalidate-cloudfront-action <<-EOF > /dev/null 2>&1
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
-${AWS_REGION}
-text
-EOF
 
 # Set it here to avoid logging keys/secrets
 if [ "$DEBUG" = "1" ]; then
@@ -101,7 +76,7 @@ fi
 # Use our dedicated profile and suppress verbose messages.
 # Support v1.x of the awscli which does not have this flag
 [[ "$(aws --version)" =~ "cli/2" ]] && pagerflag="--no-cli-pager"
-aws $pagerflag --profile invalidate-cloudfront-action \
+aws $pagerflag \
   cloudfront create-invalidation \
   --distribution-id "$DISTRIBUTION" \
   --cli-input-json "file://${RUNNER_TEMP}/invalidation-batch.json"
